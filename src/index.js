10af09c3e03856e250769cfc289b681feda1c6eb
Jean-Quenault <jeanform@proton.me>
2024-02-02 17:12:03 +0100

    Update Amplify configuration and add theme provider

--- src/index.js
+++ src/index.js
@@ -5,18 +5,15 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {ThemeProvider} from "@aws-amplify/ui-react";
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
 import "@aws-amplify/ui-react/styles.css";
 import studioTheme from './ui-components/studioTheme';
+import amplifyconfig from './amplifyconfiguration.json';
 
-Amplify.configure(awsconfig);
+Amplify.configure(amplifyconfig);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+<ThemeProvider theme={studioTheme}>
+  <App />
+</ThemeProvider>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
