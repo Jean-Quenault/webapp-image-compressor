213c91c68e2c736b2b03de18de3b8b943a99a659
Jean-Quenault <jeanform@proton.me>
2024-02-02 21:30:44 +0100

    Update MarketingFooterBrand component label

--- src/ui-components/MarketingFooterBrand.jsx
+++ src/ui-components/MarketingFooterBrand.jsx
@@ -77,7 +77,7 @@ export default function MarketingFooterBrand(props) {
             position="relative"
             padding="0px 0px 0px 0px"
             whiteSpace="pre-wrap"
-            children="Products"
+            children="A propos"
             onClick={() => {
               productsOnClick();
             }}
